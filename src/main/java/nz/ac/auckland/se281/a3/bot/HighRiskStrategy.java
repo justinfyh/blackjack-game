ae9926504e6f9051388438773a72264f82c3222e
justin <justinhuang2020@gmail.com>
2022-05-15 16:12:18 +1200

    Implemented the HighRiskStrategy class

--- src/main/java/nz/ac/auckland/se281/a3/bot/HighRiskStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/bot/HighRiskStrategy.java
@@ -1,12 +1,35 @@
 package nz.ac.auckland.se281.a3.bot;
 
+import java.util.Random;
+
+import nz.ac.auckland.se281.a3.Hand;
 import nz.ac.auckland.se281.a3.Participant.Action;
 
 public class HighRiskStrategy implements BotStrategy {
 
 	@Override
 	public Action play() {
+		return null;
 
+	}
+
+	@Override
+	public Action decideAction(Hand hand) {
+		// TODO Auto-generated method stub
+		int score = hand.getScore();
+
+		if (score >= 19) {
+			return Action.HOLD;
+		}
+		return Action.HIT;
+	}
+
+	@Override
+	public int makeABet() {
+		// TODO Auto-generated method stub
+		float randomNumber = new Random().nextInt(100 - 50) + 50;
+		System.out.println((int) randomNumber);
+		return (int) randomNumber;
 	}
 
 }
