a32f81ee65321db8b6da1123c5c89473f165fe7b
justin <justinhuang2020@gmail.com>
2022-05-16 14:27:07 +1200

    Added JAVADOC to methods where necessary

--- src/main/java/nz/ac/auckland/se281/a3/bot/BotStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/bot/BotStrategy.java
@@ -5,8 +5,21 @@ import nz.ac.auckland.se281.a3.Participant.Action;
 
 public interface BotStrategy {
 
+	/**
+	 * this method takes input parameter of hand and returns the action either hit
+	 * or hold depending on the strategy instance/implementation
+	 * 
+	 * @param hand the player's hand
+	 * @return Action hit or hold
+	 */
 	Action decideAction(Hand hand);
 
+	/**
+	 * this method makes a bet which is returned as an integer and depends on the
+	 * strategy's implementation
+	 * 
+	 * @return int bet amount of player
+	 */
 	public int makeABet();
 
 }
--- src/main/java/nz/ac/auckland/se281/a3/dealer/DealerStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/dealer/DealerStrategy.java
@@ -11,8 +11,24 @@ import nz.ac.auckland.se281.a3.Player;
 
 public interface DealerStrategy {
 
+	/**
+	 * this method decides and returns the action of the dealer, either hit or hold,
+	 * depending on the strategy implementation and the two input parameters
+	 * 
+	 * @param dealerHand hand of the dealer
+	 * @param playerHand hand of the player that the dealer is targeting
+	 * @return Action either hit or hold
+	 */
 	public Action decideAction(Hand dealerHand, Hand playerHand);
 
+	/**
+	 * this method gets the target player for the dealer to win against using the
+	 * list of players and returns the hand of the targeted player based on the
+	 * strategy implementation
+	 * 
+	 * @param players list containing all the players in the game
+	 * @return Hand the hand of the player that the dealer is targeting
+	 */
 	public Hand getTargetPlayer(List<Player> players);
 
 }
